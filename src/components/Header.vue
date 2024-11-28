918454b529f13bcd212d3fb890b7953b3794989e
glitch-Staticize <static.staticize@gmail.com>
2024-11-28 13:56:21 -0500

    Added CSS code

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<style scoped>
+.topnav {
+  background-color: #3d7b22;
+  overflow: hidden;
+}
+
+.topnav b {
+  font-size: 18px;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: left;
+  padding: 14px 16px;
+  text-decoration: none;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.topnav a:hover {
+  background-color: #1a1a1a;
+  color: #00ff00;
+}
+</style>
